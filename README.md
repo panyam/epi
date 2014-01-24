ec19596dcf9fd7d96e554f1d1f2c6775efedeecf
Sriram Panyam <sri.panyam@gmail.com>
2014-01-23 21:32:34 -0800

    Adding 5.5

---
+++ README.md
@@ -0,0 +1,8 @@
+
+This is a collection of solutions for most of the problems (and counting) in the Elements 
+of Programming Interviews book in Haskell.
+
+Why Haskell?  Because I've always liked it and wanted to get back into it and while it is 
+simple enough to do things I Haskell, doing so in a way that guarantees algorithmic 
+complexities is tricky and I wanted this to be an exercise in that as well.
+
--- chapter5_primitive_types.hs
+++ chapter5_primitive_types.hs
@@ -6,11 +6,20 @@ decToBin x = reverse $ decToBin' x
     decToBin' 0 = []
     decToBin' y = let (a,b) = quotRem y 2 in [b] ++ decToBin' a
 
+clear_lowest_set_bit :: Int -> Int
 clear_lowest_set_bit x = x .&. (x - 1)
 
 lowest_set_bit :: Int -> Int
 lowest_set_bit x = x .&. complement (x - 1)
 
+lowest_set_bit_index :: Int -> Int
+lowest_set_bit_index x = floor (logBase (fromIntegral 2) (fromIntegral (lowest_set_bit x)))
+
+set_bit_positions :: Int -> [Int]
+set_bit_positions 0 = []
+set_bit_positions x = (lowest_set_bit_index x) : (set_bit_positions (clear_lowest_set_bit x))
+
+num_set_bits_simple :: Int -> Int
 num_set_bits_simple 0 = 0
 num_set_bits_simple x 
   | x .&. 1 == 1 = 1 + num_set_bits_simple (x `shiftR` 1)
@@ -34,8 +43,5 @@ power_set [] = []
 power_set alphabet = [str_from_int x | x <- [0 .. ((1::Int) `shiftL` numAlpha) - 1]]
   where 
     numAlpha = length alphabet
-    str_from_int 0 = ""
-    str_from_int x = (str_from_int (clear_lowest_set_bit x)) ++ 
-                        ([alphabet !! (lowest_bit x)])
-        where lowest_bit x = floor (logBase (fromIntegral 2) (fromIntegral (lowest_set_bit x)))
+    str_from_int x = [alphabet !! i | i <- set_bit_positions x]
 
